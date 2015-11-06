5b1f773d764155262a2044ff31b56cc53663805f
Wes Hatch <wes@apathetic-2.local>
2015-11-05 21:45:24 -0500

    still reworking how this will function

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 var express = require("express");
 var harp = require("harp");
-var builder = require("./builder.js");
+// var builder = require("./builder.js");
 var app = express();
 
 // programmatically generate each component page
-builder.clean();
-builder.build();
+// builder.clean();
+// builder.build();
 
 app.use(express.static(__dirname + "/public"));
 app.use(harp.mount(__dirname + "/public"));
