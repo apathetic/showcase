f17a0f4851fe4ba2dbf8acba398d2086c2df4bbd
wes hatch <wes.hatch@gmail.com>
2016-07-29 21:21:58 -0400

    killing babel in favour of buble

--- rollup.config.js
+++ rollup.config.js
@@ -1,29 +1,34 @@
-import babel from 'rollup-plugin-babel';
+// import babel from 'rollup-plugin-babel';
+import buble from 'rollup-plugin-buble';
 import resolve from 'rollup-plugin-node-resolve';
 import uglify from 'rollup-plugin-uglify';
 
 export default {
   entry: 'source/js/home.js',
+  dest: 'dist/js/home.min.js',
   // format: 'cjs',
   format: 'iife',
-  dest: 'dist/js/home.js',
+  // format: 'umd',
   plugins: [
-    babel({
-      exclude: [
-        'node_modules/**',
-        'source/js/lib/**'
-      ],
-      presets: ['es2015-rollup']
-    }),
     resolve({
       jsnext: true,
       main: true,
 
-
-      // Some package.json files have a `browser` field which specifies alternative files to load for people bundling
-      // for the browser. If that's you, use this option, otherwise pkg.browser will be ignored.
-      browser: false  // true
+      // Some package.json files have a `browser` field which specifies
+      // alternative files to load for people bundling for the browser. If
+      // that's you, use this option, otherwise pkg.browser will be ignored.
+      browser: false
     }),
+    buble(),
+    // babel({
+    //   babelrc: false
+    //   presets: ['es2015-rollup'],
+    //   exclude: [
+    //     'node_modules/**',
+    //     'source/js/lib/**'
+    //   ]
+    // }),
+
     uglify()
   ]
 };
--- source/js/home.js
+++ source/js/home.js
@@ -1,62 +1,63 @@
-import Scrollify from '@apatheticwes/scrollify';
+// import Scrollify from '@apatheticwes/scrollify';
 import Carousel from  '@apatheticwes/flexicarousel';
 
+// var Scrollify = require('@apatheticwes/scrollify');
+// var Carousel = require('@apatheticwes/flexicarousel');
 
 // ---- carousel ---- //
-let featured = document.querySelector('#featured');
-let bullets = featured.querySelectorAll('nav li');
-let carousel = new Carousel(featured, {
-  slides: '.slide',
-  offscreen: 1
-  // onSlide: updateNav
-});
+const featured = document.querySelector('#featured');
+const bullets = featured.querySelectorAll('nav li');
+const sections = document.querySelectorAll('main section');
+const tabs = document.querySelectorAll('#tabs li');
+let tags;
 
+const carousel = new Carousel(featured, {
+  slides: '.slide',
+  offscreen: 1,
+  onSlide: updateNav
+});
 
 
 featured.querySelector('.prev').addEventListener('click', carousel.prev);
 featured.querySelector('.next').addEventListener('click', carousel.next);
-
-Array.prototype.forEach.call(bullets, (bullet, to) => {
+Array.from(bullets, (bullet, to) => {
   bullet.addEventListener('click', (e) => { carousel.go(to); });
 });
 
 function updateNav(current) {
   for (let i = bullets.length; i--;) {
     bullets[i].classList.remove('active');
-    bullets[current].classList.add('active');
   }
+  bullets[current].classList.add('active');
 }
 
-/* * /
+/* */
 
 // ---- filters ---- //
-let sections = document.querySelectorAll('main section');
-let tabs = document.querySelectorAll('#tabs li');
-let tags;
-let i, j;
 
-Array.prototype.forEach.call(tabs, (tab) => {
+Array.from(tabs, (tab) => {
+  const filter = tab.getAttribute('data-filter');
+
   tab.addEventListener('click', function(e) {
-    let filter = tab.getAttribute('data-filter');
     let regex;
 
     e.preventDefault();
 
-    for (j = tabs.length; j--;) {
-      tabs[j].classList.remove('active');
+    for (let i = tabs.length; i--;) {
+      tabs[i].classList.remove('active');
     }
 
     tab.classList.add('active');
 
-    for (j = sections.length; j--;) {
-      tags = sections[j].getAttribute('data-tags') || '';
+    for (let i = sections.length; i--;) {
+      tags = sections[i].getAttribute('data-tags') || '';
       tags = tags.toLowerCase();
       regex = new RegExp(filter, 'i');    // ghetto, doesn't care about surrounding whitespace
 
       if (!tags.match(regex)) {
-        sections[j].classList.add('collapsed');
+        sections[i].classList.add('collapsed');
       } else {
-        sections[j].classList.remove('collapsed');
+        sections[i].classList.remove('collapsed');
       }
     }
 
